ecdaec13c4c7b5e541a75dcb71229ab7876bded2
20urc3 <94982366+20urc3@users.noreply.github.com>
2023-02-28 23:01:11 +0100

    Update server.py
    Parsing report to SQLite db

--- sekyriu/server.py
+++ sekyriu/server.py
@@ -5,7 +5,7 @@ from typing import Optional, Dict
 import numpy as np
 import pandas as pd
 import dataplane as dp
-import openai, os, sys, re, textwrap 
+import openai, os, sys, re, textwrap, sqlite3
 
 # Starting screen
 
@@ -82,7 +82,7 @@ def analyse_GPT(string):
 	Analyse funcs received with chatGPT
 	"""
 
-	string = "Modify the following code snippet by adding comment on how it works, change variable and function name for more understeable one"
+	string = "Modify the following code snippet by adding comment on how it works, change variable and function name for more understeable one and add [FUNC_START] before the snippet and newline [FUNC_END] after the snippet + string"
 	try:
 		response = openai.Completion.create(
             model="text-davinci-003",
@@ -104,12 +104,36 @@ def analyse_GPT(string):
 	f.close()
 
 
+def generate_sql(db_name, table_name, text):
+    # Connect to the database
+    conn = sqlite3.connect(db_name)
+
+    # Create a table to store the extracted functions
+    conn.execute(f'''CREATE TABLE {table_name}
+                 (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                  function TEXT NOT NULL)''')
+
+    # Define the regular expression pattern
+    pattern = r'\[START\](.*?)\[END\]'
+
+    # Find all matches in the text using the regex
+    matches = re.findall(pattern, text, re.DOTALL)
+
+    # Insert each function into the database
+    for func in matches:
+        conn.execute(f"INSERT INTO {table_name} (function) VALUES (?)", (func,))
+
+    # Commit the changes and close the connection
+    conn.commit()
+    conn.close()
+
 # Register functions with the server instance:
 
 #server.register_function(identify_vuln_GPT)
 #server.register_function(find_vulnerability)
 server.register_function(request_GPT)
 server.register_function(analyse_GPT)
+server.register_function(generate_sql)
 
 if __name__ == '__main__':
 	try:
