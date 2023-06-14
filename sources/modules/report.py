44113e2f9c52bf5960dd54244d821f555407e229
20urc3 <94982366+20urc3@users.noreply.github.com>
2023-06-14 14:05:39 +0200

    Update report.py

--- sources/modules/report.py
+++ sources/modules/report.py
@@ -3,64 +3,22 @@ import pandas as pd
 from jinja2 import Template
 
 proxy = xmlrpc.client.ServerProxy('http://localhost:13337')
-if not os.path.exists("Report"):
-    os.makedirs("Report")
-os.chdir("Report")
 
 def generate_infos():
 
   '''
-  Infos:
+  _TODO_ Infos:
       Scan summary and results
       List of identified file characteristics (e.g., file type, format, size, entry point, section headers, etc.)
       List of identified code features (e.g., function names, imports, exports, code sections, etc.)
       List of identified data features (e.g., strings, resources, metadata, etc.)
       List of identified anomalies or suspicious behavior (e.g., packers, obfuscation, anti-debugging, etc.)
       Recommendations for further analysis or actions (if necessary)
-
-      Binary Analysis reports typically focus on analyzing the behavior and characteristics of binary files, 
-      such as executable files, DLLs, and drivers. These reports may include information about the file's format, 
-      headers, sections, imports and exports, function names, data structures, and other technical details.
-
-Malware Analysis reports, on the other hand, focus on analyzing the behavior and characteristics of malicious 
-software, such as viruses, Trojans, and ransomware. These reports may include information about the malware's 
-functionality, behavior, persistence mechanisms, network communication, and other relevant details.
-
-Your report may fall under both categories if your binary scanner is designed to detect and analyze malware. 
-However, the specific category of your report will depend on the focus of your analysis and the type of information
-you are reporting
 '''
   now = datetime.datetime.now()
   date_time = now.strftime("%Y-%m-%d %H:%M:%S")
 
-def simple_source(input):
-  # Cleaning filename
-  filename = input
-  regex_pattern = r"\.[^.]+$"
-  stripped_filename = re.sub(regex_pattern, "", filename)
-
-  # Writing pcode.c file
-  source = proxy.rec_decomp()
-  f = open(f'{stripped_filename}_pcode.c', 'w')
-  for key, value in source.items():
-    f.write('%s:%s\n' % (key, value))
-  f.close()
-
-def gpt_source(input):
-
-  # Cleaning filename
-  filename = input
-  regex_pattern = r"\.[^.]+$"
-  stripped_filename = re.sub(regex_pattern, "", filename)
-
-  # Writing gpt_pcode.c file
-  source = proxy.rec_decomp()
-  f = open(f'{stripped_filename}_gpt_pcode.c', 'w')
-  for key, value in source.items():
-    f.write('%s:%s\n' % (key, value))
-  f.close()
-    
-def ploting():
+def reporting():
 
     # Define the named blocks information and decompiled functions dictionaries
     named_blocks_infos =  proxy.rec_block_infos()
@@ -70,7 +28,6 @@ def ploting():
     named_blocks_df = pd.DataFrame(named_blocks_infos, index=[0])
     decomp_function_df = pd.DataFrame(decomp_function_dict, index=[0])
 
-    # Define the Jinja2 template
     template = Template('''
     <!DOCTYPE html>
     <html>
@@ -78,9 +35,43 @@ def ploting():
       <meta charset="UTF-8">
       <title>Report</title>
       <style>
+        body {
+          margin: 0;
+          padding: 0;
+          font-family: Arial, sans-serif;
+        }
+        .container {
+          max-width: 1000px;
+          margin: 0 auto;
+          padding: 20px;
+        }
+        .block {
+          border: 1px solid #ddd;
+          margin-bottom: 20px;
+          padding: 10px;
+          cursor: pointer;
+          background-color: #E6F1F7;
+        }
+        .block h2 {
+          display: inline-block;
+          margin: 0;
+        }
+        .block .expand-icon {
+          float: right;
+          margin-left: 10px;
+          font-size: 16px;
+          line-height: 1;
+          color: #555;
+        }
+        .block-content {
+          max-height: medium;
+          overflow: auto;
+          text-align: center;
+        }
         table {
-          border-collapse: collapse;
           width: 100%;
+          border-collapse: collapse;
+          margin-bottom: 10px;
         }
         th, td {
           text-align: left;
@@ -95,20 +86,75 @@ def ploting():
           background-color: #f2f2f2;
         }
       </style>
+      <script>
+        function toggleContent(blockId) {
+          var content = document.getElementById(blockId + '-content');
+          var expandIcon = document.getElementById(blockId + '-expand-icon');
+          if (content.style.display === 'none') {
+            content.style.display = 'block';
+            expandIcon.innerHTML = '&#x25BC;';
+          } else {
+            content.style.display = 'none';
+            expandIcon.innerHTML = '&#x25B6;';
+          }
+        }
+      </script>
     </head>
     <body>
-      <h1>Named Blocks Information</h1>
-      {{ named_blocks_df.to_html(index=False) }}
-      <h1>Decompiled Functions</h1>
-      {{ decomp_function_df.to_html(index=False) }}
-    </body>
-    </html>
-    ''')
+      <div class="container">
+        <div class="block" onclick="toggleContent('named-blocks')">
+          <h2>Named Blocks Information <span id="named-blocks-expand-icon" class="expand-icon">&#x25BC;</span></h2>
+          <div id="named-blocks-content" class="block-content">
+            <table>
+              <thead>
+                <tr>
+                  {% for col in named_blocks_df.columns %}
+                    <th>{{ col }}</th>
+                  {% endfor %}
+                </tr>
+              </thead>
+              <tbody>
+                {% for _, row in named_blocks_df.iterrows() %}
+                  <tr>
+                    {% for value in row %}
+                      <td>{{ value }}</td>
+                    {% endfor %}
+                  </tr>
+                {% endfor %}
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div class="block" onclick="toggleContent('decomp-functions')">
+          <h2>Decompiled Functions <span id="decomp-functions-expand-icon" class="expand-icon">&#x25BC;</span></h2>
+          <div id="decomp-functions-content" class="block-content">
+            <table>
+              <thead>
+                <tr>
+                  {% for col in decomp_function_df.columns %}
+                    <th>{{ col }}</th>
+                  {% endfor %}
+                </tr>
+              </thead>
+              <tbody>
+                {% for _, row in decomp_function_df.iterrows() %}
+                  <tr>
+                    {% for value in row %}
+                      <td>{{ value }}</td>
+                    {% endfor %}
+                  </tr>
+                {% endfor %}
+              </tbody>
+            </table>
+          </div>
+        </div>''')
 
     # Render the template with the data
     html_report = template.render(named_blocks_df=named_blocks_df, decomp_function_df=decomp_function_df)
 
     # Write the HTML report to a file
-    f = open('report.html', 'w')
-    f.write(html_report)
-    f.close
+    with open('report.html', 'w') as f:
+        f.write(html_report)
+
+def report():
+  reporting()
