1681b799fd009cc28614ac6672141cdc2735b6ff
20urc3 <94982366+20urc3@users.noreply.github.com>
2023-06-14 14:04:51 +0200

    Update cli.py

--- sources/modules/cli.py
+++ sources/modules/cli.py
@@ -1,10 +1,15 @@
-import argparse, os, pathlib, time
+import argparse, os, pathlib, time, webbrowser
 import re, sys, threading
 
 from modules.ghidra_pilot import *
 from modules.report import *
 from modules.pcode import *
 from modules.vuln_search import *
+from modules.server import *
+
+def open_html_file(file_path):
+    webbrowser.open(file_path)
+
 
 def parsing():
 	"""
@@ -30,11 +35,7 @@ def parsing():
 	analysis_parser.add_argument('-vgpt','--vulgpt', help='Vulnerability Hunting via ChatGPT', action="store_true")
 	analysis_parser.add_argument('-m','--mal', help='Malware Analysis', action="store_true")
 	analysis_parser.add_argument('-mgpt','--malgpt', help='Malware Analysis via ChatGPT', action="store_true")
-
-	#Initiate Reporting subparser
-	report_parser = parser.add_argument_group("Reporting options")
-	#Reporting options
-	report_parser.add_argument('-r', '--report', help="Generate a One Pager report", action="store_true")
+	analysis_parser.add_argument('-be', '--binEx', help="Exporting with BinDiff", action="store_true")
 
 	#Initiate source generating parser
 	compiling_parser = parser.add_argument_group("Source generating options")
@@ -48,69 +49,95 @@ def core(args):
 	"""
 	Core module of the CLI. Responsible to start the action with the options set by the User.
 	"""
-	if args.file:
-		print("File used: " + str(args.file))
-		if os.path.isfile(args.file):
-			print("This is a file")
-			file_list = [str(args.file)]
-		elif os.path.isdir(args.file):
-			print("This is a directory")
-			fList = os.listdir(args.file)
-			file_list = [os.path.join(args.file, file) for file in fList]
 	
-	elif (args.file == None):
+	if (args.file is None):
 		# No file provided, exiting
 		print("No file provided, mandatory. Now exiting")
-		raise Exception
+		pass
 
-	if not os.path.exists("output"):
-		os.makedirs("output")
+	else:
 
-	for file in file_list:
+		# Start Server
+		server_thread = XMLServerThread()
+		server_thread.start()
 
-		if args.decompile:
-			# Decompile the file provided
-			thread1 = threading.Thread(target=decompiling(file,))
-			thread1.start()
-			thread1.join()
+		if os.path.isfile(args.file):
+			print("File used: "+ str(args.file))
+			file_list = [str(args.file)]
+
+		elif os.path.isdir(args.file):
+			print("File used: "+ str(args.file))
+			fList = os.listdir(args.file)
+			file_list = [str(args.file) + '/'+str(element) for element in fList]
+
+		if not os.path.exists("output"):
+			os.makedirs("output")
+
+		for file in file_list:
+
+			if args.binEx:
+				# Exporting file with BinDiff
+				thread10 = threading.Thread(target=binexporting(file))
+				thread10.start()
+				thread10.join()
 			
-			if not args.GPT:
-				# Generating the Pseudo-code file
-				thread2 = threading.Thread(target=simple_source(file))
-				thread2.start()
-				thread2.join()
+			if args.decompile:
+				# Decompile the file provided
+				thread1 = threading.Thread(target=decompiling(file))
+				thread1.start()
+				thread1.join()
 				
-			elif args.GPT:
-				# Generationg GPT commented Pseudo-code file
-				thread3 = threading.Thread(target=gpt_source(file))
-				thread3.start()
-				thread3.join()
+				os.chdir("output")
 
-		if args.vul:
-			# Vulnerability detection
-			thread4 = threading.Thread(target=vuln_hunt())
-			thread4.start()
-			thread4.join()
+				if  args.GPT:
+					# Generating the Pseudo-code file
+					thread2 = threading.Thread(target=gpt_source(file))
+					thread2.start()
+					thread2.join()
 
-		if args.vulgpt:
-			# Vulnerability detection with ChatGPT
-			print("Vulnerability hunting via ChatGPT")
+				else:
+					# Generating GPT commented Pseudo-code file
+					thread3 = threading.Thread(target=simple_source(file))
+					thread3.start()
+					thread3.join()
 
-		if args.mal:
-			# Malware analysis 
-			print("Malware analysis via Pattern Recognition and Behaviour Analysis")
+				thread9 = threading.Thread(target=report())
+				thread9.start()
+				thread9.join()
 
-		if args.malgpt:
-			# Malware analysis with ChatGPT
-			print("Malware analysis via ChatGPT")
+				if args.vul:
+					# Vulnerability detection
+					thread4 = threading.Thread(target=vuln_hunt())
+					thread4.start()
+					thread4.join()
 
-		if args.report:
-			# Generating the report	
-			print("Generating the report.")
-			thread6 = threading.Thread(target=report())
-			thread6.start()
-			thread6.join()
+				if args.vulgpt:
+					# Vulnerability detection with ChatGPT
+					thread5 = threading.Thread(target=vuln_gpt(file))
+					thread5.start()
+					thread5.join()
+	
+				if args.mal:
+					# Malware analysis
+					pass 
+		
+				if args.malgpt:
+					# Malware analysis with ChatGPT
+					thread7 = threading.Thread(target=mal_gpt(file))
+					thread7.start()
+					thread7.join()
+			
+				if args.compile:
+					# Attempt to compile the Pcode source file
+					thread8 = threading.Thread(target=compile())
+					thread8.start()
+					thread8.join()
 
-		if args.compile:
-			# Attempt to compile the Pcode source file
-			print("Compiling the pseudo source code generated")
+
+				# Open Report
+				html_file_path = str(os.getcwd())+"/report.html"
+				open_html_file(html_file_path)	
+				
+			# Exiting server
+			server_thread.stop()
+			server_thread.join()
