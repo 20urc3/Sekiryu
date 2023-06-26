308bf2b90d89ccb574b812ccfdce3e97de93e1ae
20urc3 <94982366+20urc3@users.noreply.github.com>
2023-06-26 09:13:36 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,16 +9,18 @@ setup(
     url='https://github.com/20urc3/Sekiryu',
     packages=find_packages(),
     install_requires=[
-        dataplane==0.0.20
-        Jinja2==3.1.2
-        numpy==1.24.2
-        openai==0.26.5
-        pandas==1.5.3
-        setuptools==59.6.0
-    ],
+        'dataplane==0.0.20',
+        'Jinja2==3.1.2',
+        'numpy==1.24.2',
+        'openai==0.26.5',
+        'pandas==1.5.3',
+        'setuptools==59.6.0'
+    ]'''
+    ,
     entry_points={
         'console_scripts': [
             'sekiryu = sources.sekiryu:main'
         ]
     }
+'''
 )
